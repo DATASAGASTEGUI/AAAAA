bdad4ac9292b20ec862d9456df4e006588f14fb5
DATASAGASTEGUI <datasagamadrid@gmail.com>
2023-04-12 12:06:44 +0200

    SNAPSHOT 1

---
+++ inicio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+git init
+git config user.name "DATASAGASTEGUI"
+git config user.email "datasagamadrid@gmail.com"
+git config user.password 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
+git add .
+git commit -m "SNAPSHOT 1"
+git remote add origin "https://github.com/DATASAGASTEGUI/AAAAA.git"
+git branch -M main
+git push -u origin main
